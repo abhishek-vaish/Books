da3b24fc8107730f79d09ba5c548a51640bc8794
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2019-09-19 22:56:19 +0530

    Create Getter Setter

--- src/solution/Books.java
+++ src/solution/Books.java
@@ -5,3 +5,27 @@ public class Books {
     private String authorName;
     private long ISBN;
 
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public long getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(long ISBN) {
+        this.ISBN = ISBN;
+    }
+
